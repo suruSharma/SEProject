b7e91dae591a2f28e5f2a32e1a82231a703a54ef
suruSharma <suruchi.sharma7389@gmail.com>
2016-05-09 02:58:21 -0400

    Added Household functionality

--- finApp/finApp.py
+++ finApp/finApp.py
@@ -23,7 +23,18 @@ def getProfileInformation(userId):
     return profileInfo  
     
 def profile_key(userId):
-    return ndb.Key('Profile', userId)   
+    return ndb.Key('Profile', userId)
+
+def household_key(householdId):
+    return ndb.Key('Household', householdId)
+
+class Household(ndb.Model):
+    householdId = ndb.StringProperty(indexed=True, required=True)
+    householdName = ndb.StringProperty()
+    
+class HouseholdMembers(ndb.Model):
+    householdId = ndb.StringProperty(indexed=True, required=True)
+    members = ndb.StringProperty()
     
 class UserProfile(ndb.Model):
     userId = ndb.IntegerProperty(indexed=True, required=True)
@@ -33,15 +44,46 @@ class UserProfile(ndb.Model):
     salary = ndb.IntegerProperty()
     currency = ndb.StringProperty()
     nickName = ndb.StringProperty()
+    company = ndb.StringProperty()
+    households = ndb.StringProperty(repeated=True)
     
 def encode(s):
     return abs(hash(s)) % (10 ** 8)
 
+class AddHousehold(webapp2.RequestHandler):
+    def get(self):
+        logging.info("Inside get of Add Household")
+    def post(self):
+        template = JINJA_ENVIRONMENT.get_template('household.html')
+        householdName = self.request.get('householdName')
+        householdId = addHousehold(householdName)
+        memberCount = self.request.get('count')
+        for i in range(0, int(memberCount)):
+            memberName = self.request.get('name'+str(i))
+            memberUsername = self.request.get('email'+str(i))
+            isEarning = self.request.get('earning'+str(i))
+            if(isEarning is None or not isEarning):
+                isEarning = "no"
+            else:
+                isEarning = "yes"
+            
+            
+                
+        
+def addHousehold(householdName):
+    householdId = str(uuid.uuid4())
+    household = Household(parent=household_key(householdId))
+    household.householdId = householdId
+    household.householdName = householdName
+    
+    household.put()
+    
+    return householdId
+        
 class UpdateProfile(webapp2.RequestHandler):
     def get(self):
         logging.info("Inside get of Updateprofile")
     def post(self):
-        logging.info("Inside update profile")
         template = JINJA_ENVIRONMENT.get_template('profile.html')
         error = None
         user = users.get_current_user()
@@ -52,12 +94,7 @@ class UpdateProfile(webapp2.RequestHandler):
         currency = self.request.get('currency')
         userId = self.request.get('userId')
         nickName  =self.request.get('nickName')
-        
-        logging.info("Name = "+name)
-        logging.info("Designation = "+designation)
-        logging.info("Salary = "+salary)
-        logging.info("Currency = "+currency)
-        logging.info("UserId = "+userId)
+        company = self.request.get('company')
         
         profile = getProfileInformation(int(userId))
         profile[0].userId = int(userId)
@@ -67,10 +104,11 @@ class UpdateProfile(webapp2.RequestHandler):
         profile[0].salary = int(salary)
         profile[0].currency = currency
         profile[0].email = str(users.get_current_user().email())
+        profile[0].company = company
         profile[0].put()
         
         #Go back to main page. TODO : Change this to update 
-        self.redirect('/')
+        self.redirect('/loadProfile')
     
 class SaveProfile(webapp2.RequestHandler):
     def post(self):
@@ -85,12 +123,7 @@ class SaveProfile(webapp2.RequestHandler):
         currency = self.request.get('currency')
         userId = self.request.get('userId')
         nickName = self.request.get('nickName')
-        
-        logging.info("Name = "+name)
-        logging.info("Designation = "+designation)
-        logging.info("Salary = "+salary)
-        logging.info("Currency = "+currency)
-        logging.info("UserId = "+userId)
+        company = self.request.get('company')
         
         profile = UserProfile(parent=profile_key(int(userId)))
         profile.userId = int(userId)
@@ -100,25 +133,22 @@ class SaveProfile(webapp2.RequestHandler):
         profile.salary = int(salary)
         profile.currency = currency
         profile.email = str(users.get_current_user().email())
+        profile.company = company
         profile.put()
         
         #Go back to main page. TODO : Change this to update 
-        self.redirect('/')
+        self.redirect('/loadProfile')
     
 class LoadProfile(webapp2.RequestHandler):
     def get(self):
-        logging.info("Inside Profile Page")
         user = users.get_current_user()
         
         if user:
             userCode = encode(user.email())
             profileInfo = getProfileInformation(userCode)
-            logging.info("Found a user inside Profile Page")
             url = users.create_logout_url(self.request.uri)
             if profileInfo is None or not profileInfo:
                 #The user is not present in the system yet
-                logging.info("Email = "+user.email())
-                logging.info("Profile Info not found")
                 template_values = {
                 'user': user.nickname(),
                 'url': url,
@@ -140,21 +170,65 @@ class LoadProfile(webapp2.RequestHandler):
                 'userId' : userCode,
                 'button' : 'UPDATE',
                 'action' : 'updateProfile',
-                'nickName' : profileInfo[0].nickName
+                'nickName' : profileInfo[0].nickName,
+                'company' : profileInfo[0].company
                 }
                 
             template_values = template_values
             template = JINJA_ENVIRONMENT.get_template('profile.html')
             self.response.write(template.render(template_values))
         else:
-            logging.info("User not found. Loading Landing page")
             template_values = {
                 'url' : users.create_login_url(self.request.uri)
             }
             template = JINJA_ENVIRONMENT.get_template('landing.html')
             self.response.write(template.render(template_values))
             
+class CreateHousehold(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        
+        if user:
+            userCode = encode(user.email())
+            profileInfo = getProfileInformation(userCode)
+            url = users.create_logout_url(self.request.uri)
+            if profileInfo is None or not profileInfo:
+                #The user is not present in the system yet
+                template_values = {
+                'user': user.nickname(),
+                'url': url,
+                'email' : user.email(),
+                'userId' : userCode,
+                'button' : 'SAVE',
+                'action' : 'saveProfile'
+                }
+            else:
+                template_values = {
+                'user': user.nickname(),
+                'url': url,
+                'name' : profileInfo[0].name,
+                'designation' : profileInfo[0].designation,
+                'salary' : profileInfo[0].salary,
+                'currency' : profileInfo[0].currency,
+                'email' : profileInfo[0].email,
+                'userId' : userCode,
+                'button' : 'UPDATE',
+                'action' : 'updateProfile',
+                'nickName' : profileInfo[0].nickName,
+                'company' : profileInfo[0].company
+                }
+                
+            template_values = template_values
+            template = JINJA_ENVIRONMENT.get_template('household.html')
+            self.response.write(template.render(template_values))
+        else:
+            template_values = {
+                'url' : users.create_login_url(self.request.uri)
+            }
+            template = JINJA_ENVIRONMENT.get_template('landing.html')
+            self.response.write(template.render(template_values))
             
+  
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
@@ -184,5 +258,7 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/loadProfile', LoadProfile),
     ('/saveProfile', SaveProfile),
-    ('/updateProfile', UpdateProfile)
+    ('/updateProfile', UpdateProfile),
+    ('/createHousehold', CreateHousehold),
+    ('/addHousehold', AddHousehold)
 ], debug=True)
